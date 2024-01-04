a201d7c4ab24a5ed546e703e6efc9a5a89388e70
“Shubham3795” <shubham113795@gmail.com>
2024-01-04 23:55:08 +0530

    frontend setup : very initial login page

--- online_quiz_maker/frontend/src/App.js
+++ online_quiz_maker/frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [userCred,setUserCred] = useState({email:"",password:""});
+  
+  const login=()=>{
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type='email' placeholder='user@gmail.com' ></input><br/>
+        <input type='password' placeholder='password' ></input><br/>
+        <button type='submit' onClick={login}>Submit</button>
+      </form>
     </div>
   );
 }
